25f30654ff495815d2b35c20e7617dbd1d7fb752
Keloysius Mak <sius_max@hotmail.com>
2017-10-08 18:47:47 +0800

    Fix TravisCI bugs

--- src/test/java/seedu/address/logic/commands/ViewAliasCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewAliasCommandTest.java
@@ -1,13 +1,11 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.ViewAliasCommand.MESSAGE_SUCCESS;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-import seedu.address.commons.events.ui.ViewAliasRequestEvent;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
 public class ViewAliasCommandTest {
--- src/test/java/seedu/address/ui/ViewAliasWindowTest.java
+++ src/test/java/seedu/address/ui/ViewAliasWindowTest.java
@@ -1,23 +1,11 @@
 package seedu.address.ui;
 
-import static org.junit.Assert.assertEquals;
-import static seedu.address.ui.HelpWindow.USERGUIDE_FILE_PATH;
-
-import java.net.URL;
-
 import org.junit.Before;
-import org.junit.Test;
 import org.testfx.api.FxToolkit;
 
-import guitests.guihandles.HelpWindowHandle;
 import guitests.guihandles.ViewAliasWindowHandle;
 import javafx.stage.Stage;
-import seedu.address.commons.core.Config;
 import seedu.address.logic.Logic;
-import seedu.address.logic.LogicManager;
-import seedu.address.model.Model;
-import seedu.address.model.UserPrefs;
-import seedu.address.storage.Storage;
 
 public class ViewAliasWindowTest extends GuiUnitTest {
 

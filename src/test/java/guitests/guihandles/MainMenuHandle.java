a251a880ef8f7adddc4cf56a3b5c761a476eab53
Keloysius Mak <sius_max@hotmail.com>
2017-10-08 20:22:52 +0800

    Fix TravisCI bugs

--- src/test/java/guitests/guihandles/MainMenuHandle.java
+++ src/test/java/guitests/guihandles/MainMenuHandle.java
@@ -35,7 +35,7 @@ public class MainMenuHandle extends NodeHandle<Node> {
      * Opens the {@code ViewAliasWindow} using the menu bar in {@code MainWindow}.
      */
     public void openViewAliasWindowUsingMenu() {
-        clickOnMenuItemsSequentially("F2");
+        clickOnMenuItemsSequentially("Tools", "F2");
     }
 
     /**

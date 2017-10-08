45f9bf3dcec1bb20fbbee39201bf0a0fbdbf38c2
Keloysius Mak <sius_max@hotmail.com>
2017-10-08 20:29:39 +0800

    Rework menuBar

--- src/test/java/guitests/guihandles/MainMenuHandle.java
+++ src/test/java/guitests/guihandles/MainMenuHandle.java
@@ -19,7 +19,7 @@ public class MainMenuHandle extends NodeHandle<Node> {
      * Opens the {@code HelpWindow} using the menu bar in {@code MainWindow}.
      */
     public void openHelpWindowUsingMenu() {
-        clickOnMenuItemsSequentially("Tools", "F1");
+        clickOnMenuItemsSequentially("Help", "F1");
     }
 
     /**
@@ -35,7 +35,7 @@ public class MainMenuHandle extends NodeHandle<Node> {
      * Opens the {@code ViewAliasWindow} using the menu bar in {@code MainWindow}.
      */
     public void openViewAliasWindowUsingMenu() {
-        clickOnMenuItemsSequentially("Tools", "F1", "F2");
+        clickOnMenuItemsSequentially("Alias", "F2");
     }
 
     /**

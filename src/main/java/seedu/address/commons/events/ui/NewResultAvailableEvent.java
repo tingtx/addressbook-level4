886735851f5f55b13c6963c55fce81bb7d226b37
quanle1994 <quan_le@u.nus.edu>
2017-10-09 18:59:29 +0800

    edit Error Command

--- src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
+++ src/main/java/seedu/address/commons/events/ui/NewResultAvailableEvent.java
@@ -8,13 +8,9 @@ import seedu.address.commons.events.BaseEvent;
 public class NewResultAvailableEvent extends BaseEvent {
 
     public final String message;
-    public boolean isError = false;
 
     public NewResultAvailableEvent(String message) {
         this.message = message;
-        if (message.equals("Unknown command")){
-            isError = true;
-        }
     }
 
     @Override
--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -36,7 +36,7 @@ public class ResultDisplay extends UiPart<Region> {
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         Platform.runLater(() -> displayed.setValue(event.message));
-        if (event.isError){
+        if (event.message.equals("Unknown command")){
             resultDisplay.getStyleClass().add("error");
         } else {
             resultDisplay.getStyleClass().remove("error");

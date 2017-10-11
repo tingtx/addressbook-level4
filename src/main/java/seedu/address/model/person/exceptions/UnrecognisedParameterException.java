59642a626db6f0f481f52577b4272548391cacec
quanle1994 <quan_le@u.nus.edu>
2017-10-11 19:22:00 +0800

    merge command error

---
+++ src/main/java/seedu/address/model/person/exceptions/UnrecognisedParameterException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.person.exceptions;
+
+/**
+ * Signals that the order operation has the wrong parameter.
+ */
+public class UnrecognisedParameterException extends Exception {
+}

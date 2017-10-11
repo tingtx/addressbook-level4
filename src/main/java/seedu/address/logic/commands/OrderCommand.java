5ef0d84e84b8f12b3aea3ea89f99743673f016fe
quanle1994 <quan_le@u.nus.edu>
2017-10-11 10:29:24 +0800

    modify sort

--- src/main/java/seedu/address/logic/commands/OrderCommand.java
+++ src/main/java/seedu/address/logic/commands/OrderCommand.java
@@ -7,16 +7,17 @@ import seedu.address.logic.commands.exceptions.CommandException;
  */
 public class OrderCommand extends UndoableCommand {
 
-    private String orderParameter;
     public static final String COMMAND_WORD = "order";
     public static final String COMMAND_ALIAS = "o";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Order the Address Book based on a specified parameter. "
-            + "This will affect the indices of the persons and the commands using this number, e.g. delete command.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Order the Address Book based on a parameter. "
+            + "This will affect the indices of persons and the commands using this number, e.g. delete command.\n"
             + "Parameters: PARAMETER (NAME, ADDRESS)\n"
             + "Example: " + COMMAND_WORD + " NAME";
 
     public static final String MESSAGE_SORT_SUCCESS = "Address Book has been sorted";
+
+    private String orderParameter;
 
     public OrderCommand(String orderParameter) {
         this.orderParameter = orderParameter;

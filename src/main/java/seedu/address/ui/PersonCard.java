ad44c666f50ae51a22d6f6566a8c85325d7b3a7e
quanle1994 <quan_le@u.nus.edu>
2017-10-12 18:02:57 +0800

    edit imports

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,7 +1,6 @@
 package seedu.address.ui;
 
 import javafx.beans.binding.Bindings;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
@@ -11,10 +10,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.ReadOnlyPerson;
 import seedu.address.model.tag.Tag;
-
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.security.MessageDigest;
 
 /**
  * An UI component that displays information of a {@code Person}.

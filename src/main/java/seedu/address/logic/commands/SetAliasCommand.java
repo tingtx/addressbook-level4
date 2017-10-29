98727f9ee10bb17fb277182a070bc8af9bfe9808
Keloysius Mak <sius_max@hotmail.com>
2017-10-29 19:06:44 +0800

    Collate Essentials - batch script and jar

--- src/main/java/seedu/address/logic/commands/SetAliasCommand.java
+++ src/main/java/seedu/address/logic/commands/SetAliasCommand.java
@@ -1,3 +1,4 @@
+//@@author keloysiusmak
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
--- src/main/java/seedu/address/logic/commands/ViewAliasCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewAliasCommand.java
@@ -1,3 +1,4 @@
+//@@author keloysiusmak
 package seedu.address.logic.commands;
 
 import seedu.address.commons.core.EventsCenter;

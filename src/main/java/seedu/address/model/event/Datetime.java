9656a96513c15fda3b7ce11ca3e72c949d032466
Kai Yu Quek <kaiyu92@hotmail.com>
2017-11-03 22:55:45 +0800

    no message

--- src/main/java/seedu/address/model/event/Datetime.java
+++ src/main/java/seedu/address/model/event/Datetime.java
@@ -13,12 +13,6 @@ public class Datetime {
 
     public static final String MESSAGE_DATETIME_CONSTRAINTS =
             "Event datetime should only contain dd-mm-yyyy hhmm";
-    /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final String DATETIME_VALIDATION_REGEX =
-            "((0[1-9])|([012][0-9])|(3[01]))-(0[1-9]|1[012])-\\d\\d\\d\\d (([0-1][0-9])|(2[0-3]))[0-5][0-9]";
 
     private static final int VALID_DATETIME_LENGTH = 15;
 

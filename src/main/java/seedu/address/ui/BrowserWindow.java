3ca8a0d36766cbd0019ff9f6e77d52e4969e065c
Kai Yu Quek <kaiyu92@hotmail.com>
2017-11-12 16:53:15 +0800

    no message

--- src/main/java/seedu/address/ui/BrowserWindow.java
+++ src/main/java/seedu/address/ui/BrowserWindow.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import java.net.URL;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -11,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.commons.util.FxViewUtil;
@@ -21,6 +23,7 @@ import seedu.address.model.person.ReadOnlyPerson;
  */
 public class BrowserWindow extends UiPart<Region> {
 
+    public static final String DEFAULT_PAGE = "default.html";
     public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
     public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
     private static final String ICON = "/images/help_icon.png";
@@ -44,6 +47,8 @@ public class BrowserWindow extends UiPart<Region> {
         dialogStage.setMinHeight(400);
         dialogStage.setMinWidth(600);
         FxViewUtil.setStageIcon(dialogStage, ICON);
+
+        loadDefaultPage();
         registerAsAnEventHandler(this);
     }
 
@@ -60,6 +65,14 @@ public class BrowserWindow extends UiPart<Region> {
      */
     public void loadPage(String url) {
         Platform.runLater(() -> browser.getEngine().load(url));
+    }
+
+    /**
+     *  Loads a default HTML file with a background that matches the general theme.
+     */
+    private void loadDefaultPage() {
+        URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        loadPage(defaultPage.toExternalForm());
     }
 
     /**
--- src/test/java/seedu/address/ui/BrowserWindowTest.java
+++ src/test/java/seedu/address/ui/BrowserWindowTest.java
@@ -4,8 +4,10 @@ import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.ui.BrowserWindow.DEFAULT_PAGE;
 import static seedu.address.ui.BrowserWindow.GOOGLE_SEARCH_URL_PREFIX;
 import static seedu.address.ui.BrowserWindow.GOOGLE_SEARCH_URL_SUFFIX;
+import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 
 import java.net.URL;
 
@@ -15,6 +17,7 @@ import org.testfx.api.FxToolkit;
 
 import guitests.guihandles.BrowserWindowHandle;
 import javafx.stage.Stage;
+import seedu.address.MainApp;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 
 public class BrowserWindowTest extends GuiUnitTest {
@@ -31,11 +34,16 @@ public class BrowserWindowTest extends GuiUnitTest {
         guiRobot.interact(() -> browserWindow = new BrowserWindow());
         Stage browserWindowStage = FxToolkit.setupStage((stage) -> stage.setScene(browserWindow.getRoot().getScene()));
         FxToolkit.showStage();
+
         browserWindowHandle = new BrowserWindowHandle(browserWindowStage);
     }
 
     @Test
     public void display() throws Exception {
+
+        // default web page
+        URL expectedDefaultPageUrl = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
+        assertEquals(expectedDefaultPageUrl, browserWindowHandle.getLoadedUrl());
 
         postNow(selectionChangedEventStub);
         URL expectedPersonUrl = new URL(GOOGLE_SEARCH_URL_PREFIX

15f64879962b0023ec1abc37ffa9285cd408c7d5
Kai Yu Quek <kaiyu92@hotmail.com>
2017-11-12 17:30:23 +0800

    no message

--- src/test/java/seedu/address/ui/BrowserWindowTest.java
+++ src/test/java/seedu/address/ui/BrowserWindowTest.java
@@ -1,12 +1,8 @@
 package seedu.address.ui;
 
-import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
 import static org.junit.Assert.assertEquals;
-import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.ui.BrowserWindow.DEFAULT_PAGE;
-import static seedu.address.ui.BrowserWindow.GOOGLE_SEARCH_URL_PREFIX;
-import static seedu.address.ui.BrowserWindow.GOOGLE_SEARCH_URL_SUFFIX;
 import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 
 import java.net.URL;
